1697508a83807b7af1d0a46637bba0b8e9fe2856
tjdaniel1 <robot2daniel@gmail.com>
2024-07-20 04:20:17 +0900

    Feat: 유저가 작성한 전체 리뷰 조회 기능 추가 #7 토큰을 파싱하여 해당 유저가 작성한 전체 리뷰를 조회합니다.

--- src/main/java/com/T82/review/domain/repository/ReviewRepository.java
+++ src/main/java/com/T82/review/domain/repository/ReviewRepository.java
@@ -7,6 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     Review findByUserAndEventInfo(User user, EventInfo eventInfo);
+    List<Review> findAllByUser(User user);
 }
--- src/main/java/com/T82/review/service/ReviewService.java
+++ src/main/java/com/T82/review/service/ReviewService.java
@@ -5,8 +5,11 @@ import com.T82.review.domain.dto.response.ReviewResponse;
 import com.T82.review.domain.entity.Review;
 import com.T82.review.global.utils.TokenInfo;
 
+import java.util.List;
+
 public interface ReviewService {
     void addReview(TokenInfo tokenInfo, AddReviewRequest addReviewRequest);
+    List<ReviewResponse> getAllReviews(TokenInfo tokenInfo);
     ReviewResponse getReview(TokenInfo tokenInfo, Long eventInfoId);
     void deleteReview(TokenInfo tokenInfo, Long eventInfoId);
 }
